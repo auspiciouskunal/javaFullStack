04abfcfc298d662e84f6e28e2d8c8819dad45ce8
auspiciouskunal <auspiciouskunal@gmail.com>
2024-03-06 02:23:42 +0530

    Day3

---
+++ coreJava/Day3_05_March_1/Day3_05_March_1/DataTypes.java
@@ -0,0 +1,14 @@
+package Day3_05_March_1;
+public class DataTypes {
+    public static void main(String[] args){
+        byte a = 'a';
+        System.out.println("This is byte: "+a);
+
+        long l = 1234567890987l;
+        System.out.print("This is long: "+l);
+        long l2 = 12;
+        System.out.println("This is long: "+l2);
+        System.out.println("This is long type: "+typeOf(l2));
+    }
+    
+}
